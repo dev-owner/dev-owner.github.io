56651bf14d601a2f85b022c242a952fbf554baeb
dev-owner <yureka1112@gmail.com>
2023-01-08 21:24:51 +0900

    add Google Search Console Verification meta code

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -48,6 +48,11 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <meta name="twitter:site" content="@사용자이름" />
         <meta name="twitter:creator" content="@사용자이름" />
 
+        <meta
+          name="google-site-verification"
+          content="3fZBl0qGT1FPNNPKuW8K4zu4I475C4YolT11VHusyTU"
+        />
+
         <html lang="ko" />
       </Helmet>
       <GlobalStyle />
